3730084b59d5a6c926eb17c5f31ce2aba62323bf
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-13 15:34:16 +0800

    Day2(AM) (23. ReactFacts Project - Navbar & Styling)

--- 01_Static_pages/project1_ReactFacts/react-facts/src/components/Navbar.jsx
+++ 01_Static_pages/project1_ReactFacts/react-facts/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import reactIcon from '../assets/React-Logo.png';
+import './css/Navbar.css'
 
 export default function Navbar() {
     return (
-        <nav>
-            <img src={reactIcon} alt='react-logo' />
-            <span>ReactFacts</span>
-        </nav>
+        <header>
+            <nav className='nav-bar'>
+                <img src={reactIcon} alt='react-logo' className='react-logo'/>
+                <span className='nav-title'>ReactFacts</span>
+            </nav>
+        </header>
     )
 }
