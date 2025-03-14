3c858515107a344094ab1e7d0f6031e1ac53f64d
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-14 16:12:27 +0800

    Day3(AM) (21. Travel Journal - Map Entry components)

--- 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/App.jsx
+++ 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/App.jsx
@@ -2,25 +2,24 @@ import Header from './components/Header'
 import Entry from './components/Entry'
 import './App.css'
 
-import fuji from './assets/imgs/fuji.png'
+import data from './data/data.js'
 export default function App() {
+    const items = data.map((item) => {
+        const { id, img, title, country, googleMapsLink, dates, text } = item;
+        return (<Entry
+            img={img}
+            country={country}
+            googleMapsLink={googleMapsLink}
+            title={title}
+            dates={dates}
+            text={text}
+        />)
+    });
     return (
         <>
             <Header />
             <main className="container">
-                <Entry 
-                img = {{
-                    url:fuji,
-                    alt:'mount-fuji'
-                }}
-                name = 'JAPAN'
-                googleLink = "https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
-                title = 'Mount Fuji'
-                content = {{
-                    date:'12 Jan, 2021 - 24 Jan, 2021',
-                    main: 'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.'
-                }}
-                />
+                {items}
             </main>
         </>
     )
--- 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/components/Entry.jsx
+++ 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/components/Entry.jsx
@@ -3,24 +3,23 @@ import marker from '../assets/marker.png'
 
 import './css/Entry.css'
 export default function Entry(props) {
-    const { img, name, googleLink, title, content } = props;
-    const { url, alt } = img;
-    const { date, main } = content;
+    const { img, country, googleMapsLink, title, dates, text } = props;
+    const { src, alt } = img;
     return (
         <article className='japan-content'>
-            <img src={url} alt={alt} className='fuji-img' />
+            <img src={src} alt={alt} className='fuji-img' />
             <div className='main-content'>
                 <div className='main-content-1'>
                     <img src={marker} alt="marker" className='marker-img' />
-                    <span>{name}</span>
-                    <a href={googleLink}>View on Google Maps</a>
+                    <span>{country}</span>
+                    <a href={googleMapsLink}>View on Google Maps</a>
                 </div>
                 <p className='main-content-2'>
                     {title}
                 </p>
                 <p className='main-content-3'>
-                    <span>{date}</span>
-                    {main}
+                    <span>{dates}</span>
+                    {text}
                 </p>
             </div>
         </article>
