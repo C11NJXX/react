b870f3e28a47bbd114ceed9c338823adcd169dfa
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-14 15:23:12 +0800

    Day3(AM) (16. Pass props to Entry component)

--- 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/components/Entry.jsx
+++ 02_Data_Driven_React/project2_TravelJournal/travel-journal/src/components/Entry.jsx
@@ -1,23 +1,26 @@
 import marker from '../assets/marker.png'
-import fuji from '../assets/imgs/fuji.png'
+
 
 import './css/Entry.css'
-export default function Entry() {
+export default function Entry(props) {
+    const { img, name, googleLink, title, content } = props;
+    const { url, alt } = img;
+    const { date, main } = content;
     return (
         <article className='japan-content'>
-            <img src={fuji} alt="mount-fuji" className='fuji-img' />
+            <img src={url} alt={alt} className='fuji-img' />
             <div className='main-content'>
                 <div className='main-content-1'>
                     <img src={marker} alt="marker" className='marker-img' />
-                    <span>JAPAN</span>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
+                    <span>{name}</span>
+                    <a href={googleLink}>View on Google Maps</a>
                 </div>
                 <p className='main-content-2'>
-                    Mount Fuji
+                    {title}
                 </p>
                 <p className='main-content-3'>
-                    <span>12 Jan, 2021 - 24 Jan, 2021</span>
-                    Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.
+                    <span>{date}</span>
+                    {main}
                 </p>
             </div>
         </article>
