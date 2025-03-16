2a7ef8a00ec7d42d243e85fc305e2da772867d94
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-16 10:33:14 +0800

    Day5(AM) (56. Format recipe response)

--- 03_React_State/project3_ChefClaude/chef-claude/src/components/Recipe.jsx
+++ 03_React_State/project3_ChefClaude/chef-claude/src/components/Recipe.jsx
@@ -1,9 +1,12 @@
 import './css/Recipe.css'
+import Markdown from 'react-markdown'
+import remarkGfm from 'remark-gfm'
 export default function Recipe(props) {
     const { recipe } = props
     return (
-        <section>
-            {recipe}
+        <section className='recommend-section suggested-recipe-container' aria-live='polite'>
+            <h2>Chef Claude Recommends</h2>
+            <Markdown remarkPlugins={[remarkGfm]}>{recipe}</Markdown>
         </section>
     )
 }
