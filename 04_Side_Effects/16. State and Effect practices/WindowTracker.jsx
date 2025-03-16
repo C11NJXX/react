ba96e338b1f3b9cba8ae8257c1fcf7460bab6370
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-16 15:05:50 +0800

    Day5(PM) (16. State and Effect practices)

---
+++ 04_Side_Effects/16. State and Effect practices/WindowTracker.jsx
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+export default function WindowTracker() {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    useEffect(() => {
+        window.addEventListener('resize', () => {
+            setWindowWidth(window.innerWidth)
+        })
+    }, []);
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
