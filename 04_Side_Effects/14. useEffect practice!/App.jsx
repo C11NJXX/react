ea016d463ca55044b5b421ec139724c4fe3fb16f
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-16 12:28:15 +0800

    Day5(AM) (useEffect Practice2)

--- 04_Side_Effects/14. useEffect practice!/App.jsx
+++ 04_Side_Effects/14. useEffect practice!/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function App() {
     const [starWarsData, setStarWarsData] = React.useState({})
-    const [count, setCount] = React.useState(0)
+    const [count, setCount] = React.useState(1)
 
     /**
      * Challenge part 1:
@@ -10,16 +10,25 @@ export default function App() {
      * and save it in the starWarsData state. Make sure you don't
      * get stuck in an infinite rendering loop!
      */
+    // React.useEffect(() => {
+    //     fetch(`https://swapi.dev/api/people/${count}`)
+    //         .then(res => res.json())
+    //         .then(data => setStarWarsData(data))
+    // }, [count]);
+
     React.useEffect(() => {
-        fetch("https://swapi.dev/api/people/1")
-            .then(res => res.json())
-            .then(data => setStarWarsData(data))
-    }, []);
+        async function fetchData() {
+            const res = await fetch(`https://swapi.dev/api/people/${count}`);
+            const data = await res.json();
+            setStarWarsData(data);
+        }
+        fetchData()
+    }, [count]);
 
     return (
         <div>
-            <h2>The count is {count}</h2>
-            <button onClick={() => setCount(prevCount => prevCount + 1)}>Add</button>
+            <h2>The character is {count}</h2>
+            <button onClick={() => setCount(prevCount => prevCount + 1)}>Get next Character</button>
             <pre>{JSON.stringify(starWarsData, null, 2)}</pre>
         </div>
     )
