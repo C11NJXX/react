f2b921cf9ae03a51346930883679ccbeb3ece83a
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-17 15:01:03 +0800

    Day6(PM) (17. Tenzies - Accessibility Improvements)

--- 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
+++ 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
@@ -39,6 +39,9 @@ export default function App() {
                 width={width}
                 height={height}
             /> : null}
+            <div aria-live="polite" className="sr-only">
+                {gameWon && <p>Congratulations! You won! Press "New Game" to start again.</p>}
+            </div>
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='dies-container'>
--- 05_Capstone_Project_1_Tenzies/tenzies/src/components/Die.jsx
+++ 05_Capstone_Project_1_Tenzies/tenzies/src/components/Die.jsx
@@ -3,7 +3,12 @@ export default function Die(props) {
   const { value, isHeld, hold } = props;
 
   return (
-    <button className='die' style={{ backgroundColor: isHeld ? "#59E391" : "white" }} onClick={hold}>
+    <button className='die'
+      style={{ backgroundColor: isHeld ? "#59E391" : "white" }}
+      onClick={hold}
+      aria-pressed={isHeld}
+      aria-label={`Die with value ${value}, 
+            ${isHeld ? "held" : "not held"}`}>
       {value}
     </button>
   )
