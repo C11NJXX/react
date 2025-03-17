4ea88dd6524d15d305ebaf0c573a6cb86299e87a
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-17 10:08:57 +0800

    Day6(AM) (05. Tenzies - Map array to Die components)

--- 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
+++ 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
@@ -1,9 +1,10 @@
+import { useState } from 'react';
 import './App.css'
 import Die from './components/Die'
 import random from './utils/random'
 
 export default function App() {
-    const dies = random().map(value => <Die value={value} />);
+    const [dies, setDies] = useState(random().map((value, index) => <Die key={index} value={value} />));
     return (
         <main className="main-container">
             <div className='dies-container'>
