5a50a7bf9b08061e1e0e964476cafebe97e8d995
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-17 12:06:08 +0800

    Day6(AM) (14. Tenzies - End game - part 3)

--- 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
+++ 05_Capstone_Project_1_Tenzies/tenzies/src/App.jsx
@@ -3,8 +3,12 @@ import './App.css'
 import Die from './components/Die'
 import random, { randomNumber } from './utils/random'
 
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
+
 export default function App() {
     const [dies, setDies] = useState(random());
+    let gameWon = dies.every(die => die.isHeld) && dies.every(die => die.value === dies[0].value);
     function hold(id) {
         setDies(oldDies => oldDies.map(oldDie => oldDie.id === id ? { ...oldDie, isHeld: !oldDie.isHeld } : oldDie));
     }
@@ -22,15 +26,22 @@ export default function App() {
                 hold(die.id)
             }} />
     });
+
+    const { width, height } = useWindowSize()
+
     return (
         <main className="main-container">
+            {gameWon ? <Confetti
+                width={width}
+                height={height}
+            /> : null}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='dies-container'>
                 {diceElements}
             </div>
             <button className='toll-button' onClick={roll}>
-                Roll
+                {gameWon ? 'New Game' : 'Roll'}
             </button>
         </main>
     )
