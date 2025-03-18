598ed8f47b2cd4d634a5690d23283ea0f4dd00e6
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-18 09:29:22 +0800

    Day7(AM) (04. Assembly Endgame - Status Section)

--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react"
 import Header from "./components/Header"
+import Status from "./components/Status"
 export default function Hangman() {
   return (
     <main>
       <Header />
+      <Status />
     </main>
   )
 }
---
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/Status.jsx
@@ -0,0 +1,11 @@
+import './css/Status.css'
+export default function Status() {
+    return (
+        <section className='status-container'>
+            <div>
+                <p className='status-result'>You Win!</p>
+                <p className='status-result-text'>Well done!🎉</p>
+            </div>
+        </section>
+    )
+}
