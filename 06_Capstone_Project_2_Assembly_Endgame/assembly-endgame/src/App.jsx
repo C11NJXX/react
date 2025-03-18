da85bed6d9a258faad4378fc8bfbeee48d21eb5e
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-18 16:29:58 +0800

    Day7(PM) (25. Assembly Endgame)

--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
@@ -9,6 +9,9 @@ import './App.css'
 
 import languages from './data/languages.js'
 import getRandomWord from './data/words.js'
+
+import { useWindowSize } from 'react-use'
+import Confetti from 'react-confetti'
 export default function App() {
   //StateValue
   const [currentWord, setCurrentWord] = useState(getRandomWord);
@@ -20,17 +23,23 @@ export default function App() {
   const isGameLost = wrongGuess <= 7 ? false : true;
   const isGameOver = isGameWon || isGameLost;
   const isLatestLetterCorrect = guessedLetters[guessedLetters.length - 1] && currentWord.includes(guessedLetters[guessedLetters.length - 1]);
+
+  const { width, height } = useWindowSize();
   function addGuessedLetter(letter) {
     setGuessedLetters(prev => (prev.includes(letter) ? prev : [...prev, letter]))
   }
   return (
     <main className="main-container">
+      {isGameWon ? <Confetti
+        width={width}
+        height={height}
+      /> : undefined}
       <Header />
       <Status isGameWon={isGameWon} isGameLost={isGameLost} isGameOver={isGameOver} isLatestLetterCorrect={isLatestLetterCorrect} currentLanguage={languages[wrongGuess - 1]} wrongGuess={wrongGuess} />
       <List wrongGuess={wrongGuess} />
-      <Spell currentWord={currentWord} guessedLetters={guessedLetters} isGameLost={isGameLost}/>
+      <Spell currentWord={currentWord} guessedLetters={guessedLetters} isGameLost={isGameLost} />
       <KeyBoard currentWord={currentWord} guessedLetters={guessedLetters} addGuessedLetter={addGuessedLetter} isGameOver={isGameOver} />
-      <NewGame isGameOver={isGameOver} setCurrentWord={setCurrentWord} setGuessedLetters={setGuessedLetters}/>
+      <NewGame isGameOver={isGameOver} setCurrentWord={setCurrentWord} setGuessedLetters={setGuessedLetters} />
     </main>
   )
 }
