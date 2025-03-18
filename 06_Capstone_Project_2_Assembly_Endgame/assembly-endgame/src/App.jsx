142ea98878320d7d57c91d6ce5e87d3c9ed886e5
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-18 15:40:36 +0800

    Day7(PM) (19. Assembly Endgame - Disable keyboard when the game is over)

--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
@@ -6,6 +6,8 @@ import Spell from "./components/Spell"
 import KeyBoard from "./components/KeyBoard"
 import NewGame from "./components/NewGame"
 import './App.css'
+
+import languages from './data/languages.js'
 export default function App() {
   //StateValue
   const [currentWord, setCurrentWord] = useState('REACT');
@@ -16,13 +18,14 @@ export default function App() {
   const isGameWon = currentWord.split('').every(letter => guessedLetters.includes(letter));
   const isGameLost = wrongGuess <= 7 ? false : true;
   const isGameOver = isGameWon || isGameLost;
+  const isLatestLetterCorrect = guessedLetters[guessedLetters.length - 1] && currentWord.includes(guessedLetters[guessedLetters.length - 1]);
   function addGuessedLetter(letter) {
     setGuessedLetters(prev => (prev.includes(letter) ? prev : [...prev, letter]))
   }
   return (
     <main className="main-container">
       <Header />
-      <Status isGameWon={isGameWon} isGameLost={isGameLost} />
+      <Status isGameWon={isGameWon} isGameLost={isGameLost} isGameOver={isGameOver} isLatestLetterCorrect={isLatestLetterCorrect} currentLanguage={languages[wrongGuess-1]} />
       <List wrongGuess={wrongGuess} />
       <Spell currentWord={currentWord} guessedLetters={guessedLetters} />
       <KeyBoard currentWord={currentWord} guessedLetters={guessedLetters} addGuessedLetter={addGuessedLetter} />
--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/Status.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/Status.jsx
@@ -1,12 +1,20 @@
 import './css/Status.css'
 import clsx from 'clsx';
+import { getFarewellText } from '../data/farewell.js'
 export default function Status(props) {
-    const { isGameWon, isGameLost } = props;
+    const { isGameWon, isGameLost, isGameOver, isLatestLetterCorrect, currentLanguage } = props;
     return (
         <section className='status-container'>
             <div className={`status-color ${clsx({ ['status-color-won']: isGameWon, ['status-color-lost']: isGameLost })}`}>
-                <p className='status-result'>{isGameWon ? 'You Win!' : isGameLost ? 'Game over!' : ''}</p>
-                <p className='status-result-text'>{isGameWon ? 'Well done🎉' : isGameLost ? 'You lose!Better start learning Assembly🥰' : ''}</p>
+                {isGameOver
+                    ?
+                    <>
+                        <p className='status-result'>{isGameWon ? 'You Win!' : isGameLost ? 'Game over!' : ''}</p>
+                        <p className='status-result-text'>{isGameWon ? 'Well done🎉' : isGameLost ? 'You lose!Better start learning Assembly🥰' : ''}</p>
+                    </>
+                    :
+                    isLatestLetterCorrect ? 'Farewell HTML & CSS🫡' : isLatestLetterCorrect === undefined ? 'Farewell HTML & CSS🫡' : getFarewellText(currentLanguage.name)
+                }
             </div>
         </section>
     )
