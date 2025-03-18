81038ce1f2a1ec0dc0436a871cdeea49ef8a67be
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-18 10:56:13 +0800

    Day7(AM) (08. Assembly Endgame - Save the guessed letters)

--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import { useState } from "react"
 import Header from "./components/Header"
 import Status from "./components/Status"
 import List from './components/List'
@@ -7,13 +7,17 @@ import KeyBoard from "./components/KeyBoard"
 import NewGame from "./components/NewGame"
 import './App.css'
 export default function Hangman() {
+  const [guessedLetters, setGuessedLetters] = useState([]);
+  function addGuessedLetter(letter) {
+    setGuessedLetters(prev => (prev.includes(letter) ? prev : [...prev, letter]))
+  }
   return (
     <main className="main-container">
       <Header />
       <Status />
       <List />
       <Spell />
-      <KeyBoard />
+      <KeyBoard addGuessedLetter={addGuessedLetter} />
       <NewGame />
     </main>
   )
--- 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/KeyBoard.jsx
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/KeyBoard.jsx
@@ -1,24 +1,33 @@
 import './css/KeyBoard.css'
-import { nanoid } from 'nanoid';
-export default function KeyBoard() {
+export default function KeyBoard(props) {
+
+    const { addGuessedLetter } = props;
+
     const keyBoardLetters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
     const keyBoardLettersArray = keyBoardLetters.split('');
+    const keyBoardStyle = {
+        width: '70px',
+        height: '70px',
+        borderRadius: '10px',
+        padding: '6px',
+        borderWidth: '1px',
+        backgroundColor: '#FCBA29',
+        border: '1px solid #D7D7D7',
+        fontFamily: 'Hanken Grotesk',
+        fontWeight: 600,
+        fontSize: '30px',
+        lineHeight: '100%',
+        letterSpacing: '0%',
+        textAlign: 'center'
+    };
     const keyBoardElement = keyBoardLettersArray.map(letter => (
-        <button key={nanoid()} style={{
-            width: '70px',
-            height: '70px',
-            borderRadius: '10px',
-            padding: '6px',
-            borderWidth: '1px',
-            backgroundColor: '#FCBA29',
-            border: '1px solid #D7D7D7',
-            fontFamily: 'Hanken Grotesk',
-            fontWeight: 600,
-            fontSize: '30px',
-            lineHeight: '100%',
-            letterSpacing: '0%',
-            textAlign: 'center'
-        }}>
+        <button
+            key={letter}
+            style={keyBoardStyle}
+            onClick={() => {
+                addGuessedLetter(letter);
+            }}
+        >
             {letter}
         </button >
     ));
