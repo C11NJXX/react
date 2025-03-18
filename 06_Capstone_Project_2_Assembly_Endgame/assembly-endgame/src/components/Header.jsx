32c4bb592dc353dcf109f91267bd4c557c280b46
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-18 09:13:03 +0800

    Day7(AM) (02. Assembly Endgame - Project Planning) & (03. Assembly Endgame - Header Section)

---
+++ 06_Capstone_Project_2_Assembly_Endgame/assembly-endgame/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import './css/Header.css'
+export default function Header() {
+  return (
+    <header className='header-container'>
+        <h1 className="header-title">Assembly:Endgame</h1>
+        <p className="header-description">Guess the word within 8 attempts to keep the 
+        programming world safe from Assembly!</p>
+    </header>
+  )
+}
